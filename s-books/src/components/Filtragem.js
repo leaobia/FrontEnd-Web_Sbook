316add9813a2963d419cf2efaf23c2f6a5eda31b
BIANCA PEREIRA LEÃO <22282176@dsn127m1246733.cfp127aluno.local>
2023-08-31 15:45:57 -0300

    parte de baixo do header quase pronta, falta o modal

--- s-books/src/components/Filtragem.js
+++ s-books/src/components/Filtragem.js
@@ -1,24 +1,24 @@
 import slider from './Slider.png'
-import search from './search.png'
+//import search from './search.png'
 import './Filtragem.css'
-//import { Input } from '@chakra-ui/react'
-//import {
-   // Editable,
-   // EditableInput,
-  //  EditableTextarea,
-   // EditablePreview,
-  //} from '@chakra-ui/react'
-
-function Filtragem(){
-    return(
+import { Input, Stack, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
+import { PhoneIcon, SearchIcon } from '@chakra-ui/icons'
+function Filtragem() {
+    return (
         <div className="Filtragem">
             <div className="menuLocalContainer">
-                <button className='botaoMenu'><img src={slider} alt='ícone do botao de menu'/></button>
+                <button className='botaoMenu'><img src={slider} alt='ícone do botao de menu' /></button>
                 <span className='nomeDaCidade'>Carapicuíba</span>
             </div>
-       
+            <Stack spacing={4} className='stackSearch'>
+                <InputGroup className='inputGroupSearch' >
+                    <InputLeftElement pointerEvents='none'>
+                        <SearchIcon color='black' />
+                    </InputLeftElement>
+                    <Input type='tel' placeholder='Digite o nome do livro'/>
+                </InputGroup>
+            </Stack>
 
-            
         </div>
     )
 }
