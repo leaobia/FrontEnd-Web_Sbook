33e39557ec540fb146426ac7e9bdda48dc6a35bb
BIANCA PEREIRA LEÃO <22282176@dsn127m1246733.cfp127aluno.local>
2023-09-06 13:59:44 -0300

    abrindo e fechando

--- s-books/src/components/Header.jsx
+++ s-books/src/components/Header.jsx
@@ -6,7 +6,8 @@ import userFavorites from './img/coracao.png'
 import userChats from './img/chat.png'
 import PasswordInput from './PasswordInput';
 
-import { Input, Stack, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { Input, Stack, InputGroup, InputRightElement} from '@chakra-ui/react';
+import PasswordConfirmInput from './PasswordConfirmInput';
 import { EmailIcon } from '@chakra-ui/icons';
 
 import './css/Header.css'
@@ -15,53 +16,78 @@ import { Link } from "react-router-dom"
 
 import './css/Login.css'
 
+import './css/Cadastro.css'
+
 function Header() {
 
-    function openModal() {
-        document.getElementById('loginModal').classList.add('d-flex')
-        document.getElementById('loginModal').classList.remove('d-none')
+    function openModalPai(){
+        document.getElementById('modalPai').classList.remove('d-none')
+        document.getElementById('modalPai').classList.add('d-flex')
+
+        document.getElementById('containerCadastro').classList.add('d-none')
+        document.getElementById('containerCadastro').classList.remove('d-flex')
+
+        document.getElementById('containerLogin').classList.remove('d-none')
+        document.getElementById('containerLogin').classList.add('d-flex')
     }
 
-    function closeModal() {
-        document.getElementById('loginModal').classList.remove('d-flex')
-        document.getElementById('loginModal').classList.add('d-none')
+    function abrirContainerCadastro(){
+        document.getElementById('containerLogin').classList.remove('d-flex')
+        document.getElementById('containerLogin').classList.add('d-none')
+
+        document.getElementById('containerCadastro').classList.remove('d-none')
+        document.getElementById('containerCadastro').classList.add('d-flex')
     }
 
+    function abrirContainerLogin(){
+        document.getElementById('containerCadastro').classList.remove('d-flex')
+        document.getElementById('containerCadastro').classList.add('d-none')
 
+        document.getElementById('containerLogin').classList.remove('d-none')
+        document.getElementById('containerLogin').classList.add('d-flex')
+    }
+
+    function closeModalPai(){
+        document.getElementById('modalPai').classList.remove('d-flex')
+        document.getElementById('modalPai').classList.add('d-none')
+
+
+    }
 
     return (
 
 
         <div className="Header">
 
-            <div className='loginModal d-none' id='loginModal'>
-                <div className="containerLogin">
-                    <button onClick={closeModal} className='botaoFecharModalLogin'>X</button>
+            <div className='loginModal d-none' id='modalPai'>
+
+                <div className="containerLogin d-flex" id='containerLogin'>
+                    <button onClick={closeModalPai} className='botaoFecharModalLogin'>X</button>
                     <div className="formLogin">
                         <div className="textoLogin">
-                        <img src={logo} alt="logotipo da empresa" />
-                        <h1>Olá</h1>
-                        <h4>Bem vindo de volta</h4>
-                        <Stack spacing={4}>
-                            <InputGroup >
-                                <InputRightElement pointerEvents='none'>
-                                    <EmailIcon color='gray' />
-                                </InputRightElement>
-                                <Input
-                                    type='email'
-                                    placeholder='Email'
-                                    w={[250, 350, 400]}
-                                    h='48px'
-                                    className='inputField'
-                                    fontSize={['sm', 'md', 'lg']}
-                                />
-                            </InputGroup>
-                            <PasswordInput/>
-                            <Link className='forgotPassword'>Esqueci a senha</Link>
-                        </Stack>
+                            <img src={logo} alt="logotipo da empresa" className='imgLogo' />
+                            <h1>Olá</h1>
+                            <h4>Bem vindo de volta</h4>
+                            <Stack spacing={4}>
+                                <InputGroup >
+                                    <InputRightElement h='2.8rem' pointerEvents='none'>
+                                        <EmailIcon color='gray' />
+                                    </InputRightElement>
+                                    <Input
+                                        type='email'
+                                        placeholder='Email'
+                                        w={[250, 350, 400]}
+                                        h='48px'
+                                        className='inputField'
+                                        fontSize={['sm', 'md', 'lg']}
+                                    />
+                                </InputGroup>
+                                <PasswordInput />
+                                <Link className='forgotPassword'>Esqueci a senha</Link>
+                            </Stack>
                         </div>
 
-                        <span className='solicitandoConta'>Não tem conta? <Link className='linkCadastreAqui'>Cadastre-se aqui.</Link></span>
+                        <span className='solicitandoConta'>Não tem conta? <Link className='linkCadastreAqui' id='botaoCadastro' onClick={abrirContainerCadastro}>Cadastre-se aqui.</Link></span>
                         <button className='buttonLogar'>Entrar</button>
 
                     </div>
@@ -69,6 +95,70 @@ function Header() {
                         <img src={imageLogin} alt="imagem de um homem e uma mulher na biblioteca" />
                     </div>
                 </div>
+
+
+                <div className="containerCadastro d-none" id='containerCadastro'>
+                    <div className="containerLeft">
+                        <img src={imageLogin} alt="imagem de um homem e uma mulher na biblioteca" className='imgCadastro' />
+                    </div>
+                    <div className="formCadastro">
+                        <div className="textoCadastro">
+                            <img src={logo} alt="logotipo da empresa" className='imgLogo' />
+                            <h1>Criar Conta</h1>
+                            <Stack spacing={4}>
+                                <InputGroup >
+                                    <Input
+                                        type='text'
+                                        placeholder='Nome'
+                                        w={[250, 350, 400]}
+                                        h='48px'
+                                        className='inputField'
+                                        fontSize={['sm', 'md', 'lg']}
+                                    />
+                                </InputGroup>
+                                <InputGroup >
+                                    <Input
+                                        type='number'
+                                        placeholder='Cpf'
+                                        w={[250, 350, 400]}
+                                        h='48px'
+                                        className='inputField'
+                                        fontSize={['sm', 'md', 'lg']}
+                                    />
+                                </InputGroup>
+                                <InputGroup >
+                                    <InputRightElement h='2.8rem' pointerEvents='none'>
+                                        <EmailIcon color='gray' />
+                                    </InputRightElement>
+                                    <Input
+                                        type='email'
+                                        placeholder='Email'
+                                        w={[250, 350, 400]}
+                                        h='48px'
+                                        className='inputField'
+                                        fontSize={['sm', 'md', 'lg']}
+                                    />
+                                </InputGroup>
+                                <InputGroup >
+                                    <Input
+                                        type='date'
+                                        placeholder='Data de nascimento'
+                                        w={[250, 350, 400]}
+                                        h='48px'
+                                        className='inputField'
+                                        fontSize={['sm', 'md', 'lg']}
+                                    />
+                                </InputGroup>
+                                <PasswordInput />
+                                <PasswordConfirmInput />
+                            </Stack>
+                        </div>
+                        <button className='buttonLogar'>Entrar</button>
+                        <span className='loginConta'>Já tem uma conta? <Link className='linkCadastreAqui' id='botaoCadastro' onClick={abrirContainerLogin}>Entre aqui.</Link></span>
+                    </div>
+                    <button onClick={closeModalPai} className='botaoFecharModalCadastro'>X</button>
+                </div>
+
             </div>
 
             <div className="headerLinksContainer">
@@ -83,7 +173,7 @@ function Header() {
                 <div className='userMenuContainer'>
                     <div className='divborda'></div>
                     <div className="userMenu">
-                        <button className='userProfileButton' id='botaoLogin' onClick={openModal}><img src={userProfile} alt="icone de pessoa" /> <span>Entrar</span></button>
+                        <button className='userProfileButton' id='botaoLogin' onClick={openModalPai}><img src={userProfile} alt="icone de pessoa" /> <span>Entrar</span></button>
                         <div className='userMenuIcons'>
                             <Link to='/favoritos' className='link'><img src={userFavorites} alt="icone de coração para ver os favoritos" /></Link>
                             <Link to='/chat' className='link'><img src={userChats} alt="icone de chat para ver os chats enviados" /></Link>
@@ -92,6 +182,7 @@ function Header() {
                 </div>
             </div>
         </div>
+   
     )
 }
 
---
+++ s-books/src/components/PasswordConfirmInput.jsx
@@ -0,0 +1,31 @@
+import { Input, InputGroup, InputRightElement, Button, Icon } from '@chakra-ui/react';
+import React from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+
+function PasswordConfirmInput() {
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
+  
+    return (
+      <InputGroup size='md'>
+        <Input
+          pr='4.5rem'
+          type={show ? 'text' : 'password'}
+          placeholder='Confirmar senha'
+          w={[250, 350, 400]}
+          h="48px"
+        />
+        <InputRightElement width='2.8rem' h='3rem'>
+          <Button h='1.75rem' size='sm' onClick={handleClick}  bg='transparent'>
+          {show ? (
+            <Icon as={FaEye} color='gray.500'/>
+          ) : (
+            <Icon as={FaEyeSlash} color='gray.500' />
+          )}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    )
+  }
+
+  export default PasswordConfirmInput
