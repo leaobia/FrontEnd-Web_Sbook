2533daf36ca437988b0d55f9668638c0d5d5f43d
BIANCA PEREIRA LEÃO <22282176@dsn127m1242376.cfp127aluno.local>
2023-09-20 16:12:12 -0300

    generos

--- s-books/src/pages/Perfil.jsx
+++ s-books/src/pages/Perfil.jsx
@@ -22,14 +22,12 @@ function Perfil() {
     const [visibleLeft, setVisibleLeft] = useState(false);
     const idUser = localStorage.getItem('id_usuarioLogin')
     
-    console.log(idUser);
     //const baseUrl = 'https://app-nodejs.cyclic.cloud/'
     const baseUrl = 'http://10.107.144.7:8080/'
     useEffect(() => {
   
         axios.get(`${baseUrl}v1/sbook/usuario/${idUser}`)
           .then(response => {
-                    console.log(response);
                     console.log(response.data.dados[0]);
                     let nomeUser = response.data.dados[0].nome
                     let perfilFoto = response.data.dados[0].foto
@@ -47,6 +45,25 @@ function Perfil() {
           })
           .catch(error => {
             console.error('Erro ao obter dados do usuario', error);
+          });
+      }, []);
+
+      useEffect(() => {
+  
+        axios.get(`${baseUrl}v1/sbook/generos/${idUser}`)
+          .then(response => {
+                    let generosArray = response.data.generos_preferidos
+                    generosArray.forEach(genero => {
+                    
+                        const containerCategorias = document.getElementById('containerCategorias')
+                        const spanGenero = document.createElement('span')
+                        spanGenero.classList.add('nomeDaCidade')
+                        spanGenero.textContent = genero.nome_genero
+                        containerCategorias.append(spanGenero)
+                    });
+          })
+          .catch(error => {
+            console.error('Erro ao obter dados dos generos preferidos', error);
           });
       }, []);
 
@@ -102,7 +119,7 @@ function Perfil() {
                     <div className="generoDiv">
                         <h1>Categorias</h1>
                         <div className="containerCategorias" id='containerCategorias'>
-                        <span className='nomeDaCidade'>Gênero</span>
+                        
                         </div>
                     </div>
                 </div>
