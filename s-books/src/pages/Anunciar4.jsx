2512d12a41c24f246dff5c1a374408e50633217a
unknown <biancapeleao@gmail.com>
2023-10-07 13:58:56 -0300

    init integrar

--- s-books/src/pages/Anunciar4.jsx
+++ s-books/src/pages/Anunciar4.jsx
@@ -4,7 +4,7 @@ function Anunciar4() {
     let cidadeUsuario = localStorage.getItem('cidadeUsuario')
     let imgLivro = localStorage.getItem('dataImage')
     return (
-        <div className="queroAnunciar">
+        <div className="queroAnunciar3">
             <div className="sideBarContainer">
             <Link to='/anunciar3'>&larr;</Link>
                 <div className="menuLocalContainer">
@@ -12,9 +12,57 @@ function Anunciar4() {
                 </div>
             </div>
             <h1>Agora confira as informações do livro antes de publicar! </h1>
-            <div className="anunciarContainer">
-                    <img src={imgLivro} alt="imagem do livro" className='imagemLivro'/>
-            </div>
+      <div className="anuncioDados">
+        <img src={imgLivro} alt="foto do anuncio" className='fotoAnuncio' />
+        <div className="dadosAnuncioPrincipal">
+          <div className="esquerdaDadosAnuncio">
+          <p>kkk</p>
+         <p>Disponivel para: </p>
+         <p>kk</p>
+          </div>
+          <div className="direitaDadosAnuncio">
+        <Link to='/chat'><button className='messageButton'>Enviar mensagem</button></Link> 
+        <div className="anuncianteDados">
+          <img src='' alt="foto perfil do anunciante" className='fotoUser' />
+          <div className="nomeAnunciante">
+            <p>Max Kellermen</p>
+            <p>kkk</p>
+          </div>
+        </div>
+          </div>
+       
+        </div>
+        
+      </div>
+      <div className="descricaoContainer">
+        <h3 className='titleContainerDesc'>Descrição:</h3>
+        <p></p>
+      </div>
+      <div className="descricaoContainer">
+        <h3 className='titleContainerDesc'>Informações:</h3>
+       <div className="dadosLivro">
+        <div className="dadoLivro">
+          <h3>Ano de edição</h3>
+          <p></p>
+        </div>
+        <div className="dadoLivro">
+          <h3>Autor</h3>
+          <p></p>
+        </div>
+        <div className="dadoLivro">
+          <h3>Editora</h3>
+          <p></p>
+        </div>
+        <div className="dadoLivro">
+          <h3>Idioma</h3>
+          <p></p>
+        </div>
+        <div className="dadoLivro">
+          <h3>Estado do livro</h3>
+          <p></p>
+        </div>
+       </div>
+      </div>
         </div>
 
     )
