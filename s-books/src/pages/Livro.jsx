759cb93a5a0a6067ac01a85f75e04fa7e4d50a63
unknown <biancapeleao@gmail.com>
2023-10-07 13:22:28 -0300

    arrumando anuncio

--- s-books/src/pages/Livro.jsx
+++ s-books/src/pages/Livro.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Slider from 'react-slick';
+//import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Link } from "react-router-dom"
@@ -9,7 +9,7 @@ import '../components/css/Livro.css'
 import { Spinner } from '@chakra-ui/react'
 
 function Livro() {
-  const [images, setImages] = useState([]);
+ // const [images, setImages] = useState([]);
   let cidadeUsuario = localStorage.getItem('cidadeUsuario')
   let idPegarAnuncio = parseInt(localStorage.getItem('getAnuncioById'))
   let perfilFoto = localStorage.getItem('perfilFoto')
@@ -17,22 +17,22 @@ function Livro() {
   const [anuncio, setAnuncio] = useState([]);
   const [generos, setGeneros] = useState([]);
 
-  useEffect(() => {
-    fetch('https://picsum.photos/v2/list?page=1&limit=4') //
-      .then((response) => response.json())
-      .then((data) => {
-        const imageUrls = data.map((item) => item.download_url);
-        setImages(imageUrls);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch('https://picsum.photos/v2/list?page=1&limit=4') //
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const imageUrls = data.map((item) => item.download_url);
+  //       setImages(imageUrls);
+  //     });
+  // }, []);
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  };
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  // };
 
   useEffect(() => {
     axios.get(`${baseUrl}v1/sbook/anuncio/${idPegarAnuncio}`)
@@ -71,14 +71,15 @@ function Livro() {
                     <span className='nomeDaCidade'>{cidadeUsuario}</span>
                 </div>
             </div>
-      <Slider {...settings}>
+      {/* <Slider {...settings}>
         {images.map((imageUrl, index) => (
           <div key={index}>
             <img src={imageUrl} alt={`Imagem ${index + 1}`} />
           </div>
         ))}
-      </Slider>
+      </Slider> */}
       <div className="anuncioDados">
+        <img src={anuncio.foto[0].foto} alt="foto do anuncio" className='fotoAnuncio' />
         <div className="dadosAnuncioPrincipal">
           <div className="esquerdaDadosAnuncio">
           <p>{anuncio.anuncio.nome}</p>
