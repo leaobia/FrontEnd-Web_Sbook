fdfc07180369408c7ccf33e485ffff58da61de7d
BIANCA PEREIRA LEÃO <22282176@dsn127m1242269.cfp127aluno.local>
2023-12-01 13:20:01 -0300

    altura livros secao

--- s-books/src/socket.ts
+++ s-books/src/socket.ts
@@ -1,4 +1,4 @@
 import {io} from 'socket.io-client'
 
 export const socket = () => io('http://10.107.144.5:3001/')
-// export const socket = () => io("http://10.107.144.23:3001")
+ //export const socket = () => io("http://10.107.144.20:3001")
