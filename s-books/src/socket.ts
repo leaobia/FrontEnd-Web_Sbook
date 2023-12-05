76dc7dbf39b935db504144c6d306a9c308428a86
Thiago1223 <tifreitas10@gmail.com>
2023-12-05 00:34:21 -0300

    atualizado

--- s-books/src/socket.ts
+++ s-books/src/socket.ts
@@ -1,5 +1,9 @@
 import {io} from 'socket.io-client'
 
 // export const socket = () => io('http://10.107.144.5:3001/')
-export const socket = () => io('https://sbookapp.azurewebsites.net/')
+// export const socket = () => io('https://sbookapp.azurewebsites.net/')
  //export const socket = () => io("http://10.107.144.20:3001")
+
+export const socket = () => io("https://testewebsocketsbook.webpubsub.azure.com", {
+    path: "/clients/socketio/hubs/Hub",
+});
